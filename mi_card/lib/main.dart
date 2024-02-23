d38ac98b04b46b7a08f157764588bc5ad09274b4
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-23 21:15:17 +0300

    mi_card project starter template done

---
+++ mi_card/lib/main.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Center(
+    child: Text('Hello World'),
+  )));
+}
