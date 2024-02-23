e1a6b960dad9b5d69ed35051487bb01e055cdff2
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-23 11:43:49 +0300

    images added to the app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,15 +1,21 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
           title: Center(
               child: Text(
-            "This is the app bar",
+            "I Am Rich",
             style: TextStyle(color: Colors.white),
           )),
           backgroundColor: Colors.blueGrey[900]),
+      body: Center(
+          child: Image(
+        image: AssetImage('images/diamond.png'),
+      )),
     ),
   ));
 }
