533a702fa586fed6f18881510a446fbd51671ac2
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-23 22:55:23 +0300

    avatar photo added to the project

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -2,8 +2,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Center(
-    child: Text('Hello World'),
-  )));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'KodeMono'),
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Column(
+            children: <Widget>[
+              Container(
+                child: CircleAvatar(
+                  backgroundColor: Colors.blue[900],
+                  radius: 56.0,
+                  backgroundImage: AssetImage('images/dawit_zewdu.jpg'),
+                ),
+              )
+            ],
+          ))),
+    );
+  }
 }
