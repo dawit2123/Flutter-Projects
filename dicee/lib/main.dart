31506d7f35b5e6bafcfb473a6f1566b90fceb67e
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-24 14:48:24 +0300

    dice images displayed

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -3,21 +3,39 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
+        theme: ThemeData(fontFamily: 'Roboto'),
+        home: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.red,
+            appBar: AppBar(
+              title: Text(
+                'Dicee',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.red,
+            ),
+            body: DicePage(),
+          ),
+        )),
   );
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image(
+              image: AssetImage('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          )
+        ],
+      ),
+    );
   }
 }
