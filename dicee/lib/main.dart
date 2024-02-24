686d53f7655f305a00c53ee25de4ed7290b5f220
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-24 16:20:49 +0300

    dice app basic functionality done

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -8,32 +9,57 @@ void main() {
           child: Scaffold(
             backgroundColor: Colors.red,
             appBar: AppBar(
-              title: Text(
-                'Dicee',
-                style: TextStyle(color: Colors.white),
+              title: Center(
+                child: Text(
+                  'Dicee',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
               backgroundColor: Colors.red,
             ),
-            body: DicePage(),
+            body: Center(
+              child: DicePage(),
+            ),
           ),
         )),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int diceNumber1 = 1;
+  int diceNumber2 = 1;
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: <Widget>[
           Expanded(
+              child: FloatingActionButton.large(
             child: Image(
-              image: AssetImage('images/dice1.png'),
+              image: AssetImage('images/dice$diceNumber1.png'),
             ),
-          ),
+            onPressed: () {
+              setState(() {
+                diceNumber1 = Random().nextInt(6) + 1;
+              });
+            },
+          )),
           Expanded(
-            child: Image.asset('images/dice1.png'),
-          )
+              child: FloatingActionButton.large(
+            child: Image.asset('images/dice$diceNumber2.png'),
+            onPressed: () {
+              setState(() {
+                diceNumber2 = Random().nextInt(6) + 1;
+              });
+            },
+          ))
         ],
       ),
     );
