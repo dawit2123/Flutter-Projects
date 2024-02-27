83fbe84a901210672508a6c170e1b4846c62f473
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-27 09:28:33 +0300

    minor refactoring done

--- quizzler_app/lib/question.dart
+++ quizzler_app/lib/question.dart
@@ -1,7 +1,5 @@
 class Question {
   String questionText = '';
   bool questionAnswer = true;
-  //What does the down line of code do ?
-  // Questions(this.questionText, this.questionAnswer);
   Question(this.questionText, this.questionAnswer);
 }
