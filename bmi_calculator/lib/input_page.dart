5ca1306f2c2c8ce233131f1ae07d2b3dd7634861
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-28 15:53:50 +0300

    button shift between male and female donw

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'gender_configuration.dart';
@@ -5,6 +7,7 @@ import 'basic_container.dart';
 
 const minimumBottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inActiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
@@ -15,6 +18,18 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  void activeColorSet(String gender) {
+    if (gender == 'male') {
+      maleColor = activeCardColor;
+      femaleColor = inActiveCardColor;
+    } else if (gender == 'female') {
+      maleColor = inActiveCardColor;
+      femaleColor = activeCardColor;
+    }
+  }
+
+  Color maleColor = activeCardColor;
+  Color femaleColor = activeCardColor;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +43,26 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: <Widget>[
               Expanded(
-                child: BasicContainer(
-                  activeCardColor,
-                  GenderConfigurations(FontAwesomeIcons.mars, 'MALE'),
+                child: GestureDetector(
+                  onTap: () => {
+                    setState(() {
+                      activeColorSet('male');
+                    })
+                  },
+                  child: BasicContainer(
+                    maleColor,
+                    GenderConfigurations(FontAwesomeIcons.mars, 'MALE'),
+                  ),
                 ),
               ),
               Expanded(
-                  child: BasicContainer(activeCardColor,
-                      GenderConfigurations(FontAwesomeIcons.venus, 'FEMALE'))),
+                  child: GestureDetector(
+                onTap: () => setState(() {
+                  activeColorSet('female');
+                }),
+                child: BasicContainer(femaleColor,
+                    GenderConfigurations(FontAwesomeIcons.venus, 'FEMALE')),
+              )),
             ],
           )),
           Expanded(
